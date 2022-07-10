e5deac60c316d7f5a45e9e893955dc4bba626ee7
DeniseLeandroDeCastro <dmaria.natal@gmail.com>
2022-07-10 17:09:48 -0300

    Adicinar arquivos básicos ao repositório

---
+++ app/src/main/java/com/android/viagens/ui/activity/ListaPacotesActivity.java
@@ -0,0 +1,16 @@
+package com.android.viagens.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.android.viagens.R;
+
+public class ListaPacotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pacotes);
+    }
+}
