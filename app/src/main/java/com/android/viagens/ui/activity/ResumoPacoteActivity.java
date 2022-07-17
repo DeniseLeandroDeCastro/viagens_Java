147925446a3004d9a39cfb9a541ff83848236a77
DeniseLeandroDeCastro <dmaria.natal@gmail.com>
2022-07-17 17:46:01 -0300

    Tela de Resumo do pacote

---
+++ app/src/main/java/com/android/viagens/ui/activity/ResumoPacoteActivity.java
@@ -0,0 +1,16 @@
+package com.android.viagens.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.android.viagens.R;
+
+public class ResumoPacoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resumo_pacote);
+    }
+}
